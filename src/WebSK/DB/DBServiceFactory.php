58b377cdbda278c4abff6ba6c0f62eb58da2f50d
skulkov <sergey.kulkov@gmail.com>
2021-04-14 09:29:26 +0300

    factory cache

--- src/WebSK/DB/DBServiceFactory.php
+++ src/WebSK/DB/DBServiceFactory.php
@@ -8,24 +8,49 @@ namespace WebSK\DB;
  */
 class DBServiceFactory
 {
+
+    protected static array $db_connector_objs_arr = [];
+
     /**
      * @param array $db_config
      * @return DBService
      */
     public static function factoryMySQL(array $db_config): DBService
     {
-        $db_connector = new DBConnectorMySQL(
+        $db_settings = new DBSettings(
+            'mysql',
+            $db_config['dump_file_path']
+        );
+
+        return new DBService(
+            self::getDBConnectorMySQL($db_config),
+            $db_settings
+        );
+    }
+
+    /**
+     * @param array $db_config
+     * @return DBConnectorMySQL
+     */
+    protected static function getDBConnectorMySQL(array $db_config) : DBConnectorMySQL
+    {
+        $db_connector_hash = md5(
+            $db_config['host'] . $db_config['db_name'] . $db_config['user'] . $db_config['password']
+        );
+
+        if (isset(self::$db_connector_objs_arr[$db_connector_hash])
+            && self::$db_connector_objs_arr[$db_connector_hash] instanceof DBConnectorMySQL
+        ) {
+            return self::$db_connector_objs_arr[$db_connector_hash];
+        }
+
+        self::$db_connector_objs_arr[$db_connector_hash] = new DBConnectorMySQL(
             $db_config['host'],
             $db_config['db_name'],
             $db_config['user'],
             $db_config['password']
         );
 
-        $db_settings = new DBSettings(
-            'mysql',
-            $db_config['dump_file_path']
-        );
-
-        return new DBService($db_connector, $db_settings);
+        return self::$db_connector_objs_arr[$db_connector_hash];
     }
 }
